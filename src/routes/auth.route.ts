1d29edf308d03296cade4b52667c7ebb844c089c
TechAsish1212 <gitasish1212@gmail.com>
2026-07-09 23:26:27 +0530

    feat: add routes for login

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signup, verifyOTP } from "../controllers/user.controller";
+import { signin, signup, verifyOTP } from "../controllers/user.controller";
 
 const authRoutes=Router();
 
 authRoutes.post('/signup',signup);
+authRoutes.post('/signin',signin);
 authRoutes.post('/verify-otp',verifyOTP);
 
 export default authRoutes;
