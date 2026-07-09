f7015634516d404c9272730451675ff7a276a438
TechAsish1212 <gitasish1212@gmail.com>
2026-07-10 00:15:45 +0530

    feat: add route for get profile

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { signin, signup, verifyOTP } from "../controllers/user.controller";
+import { getPrpfile, signin, signup, verifyOTP } from "../controllers/user.controller";
+import { protect } from "../middlewares/auth.middleware";
 
 const authRoutes=Router();
 
 authRoutes.post('/signup',signup);
 authRoutes.post('/signin',signin);
 authRoutes.post('/verify-otp',verifyOTP);
+authRoutes.get('/me',protect,getPrpfile);
 
 export default authRoutes;
