7f5b0873b39ec39b6edc9365494293b86482f3e1
TechAsish1212 <gitasish1212@gmail.com>
2026-07-12 22:36:44 +0530

    feat:add signout  route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getPrpfile, signin, signup, verifyOTP } from "../controllers/user.controller";
+import { getPrpfile, signin, signout, signup, verifyOTP } from "../controllers/user.controller";
 import { protect } from "../middlewares/auth.middleware";
 
 const authRoutes=Router();
 
 authRoutes.post('/signup',signup);
 authRoutes.post('/signin',signin);
+authRoutes.post('/signout',signout);
 authRoutes.post('/verify-otp',verifyOTP);
 authRoutes.get('/me',protect,getPrpfile);
 
