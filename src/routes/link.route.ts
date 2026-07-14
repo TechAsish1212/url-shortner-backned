2f5193dc210fecb00dfb5a1673a8d5967ea9cfa1
TechAsish1212 <gitasish1212@gmail.com>
2026-07-14 23:05:11 +0530

    feat: add route for redirect likns

--- src/routes/link.route.ts
+++ src/routes/link.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { protect } from "../middlewares/auth.middleware";
-import { createShortUrl } from "../controllers/link.controller";
+import { createShortUrl, getAllUserLinks, redirectToOriginalUrl } from "../controllers/link.controller";
 
 const linkRoutes=Router();
 
 linkRoutes.post('/shorten',protect,createShortUrl);
+linkRoutes.get('/:shortCode',redirectToOriginalUrl);
 
 export default linkRoutes;
