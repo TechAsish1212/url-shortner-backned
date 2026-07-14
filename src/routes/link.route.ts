233f8263be3976a5118f205bafcfeb54f6ae1e4c
TechAsish1212 <gitasish1212@gmail.com>
2026-07-15 01:01:47 +0530

    feat: add routes single links access

--- src/routes/link.route.ts
+++ src/routes/link.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { protect } from "../middlewares/auth.middleware";
-import { createShortUrl, getAllUserLinks, redirectToOriginalUrl } from "../controllers/link.controller";
+import { createShortUrl, getAllUserLinks, getLinksDetails, redirectToOriginalUrl } from "../controllers/link.controller";
 
 const linkRoutes=Router();
 
 linkRoutes.post('/shorten',protect,createShortUrl);
 linkRoutes.get('/:shortCode',redirectToOriginalUrl);
 linkRoutes.get('/user/links',protect,getAllUserLinks);
+linkRoutes.get('/user/links/:id',protect,getLinksDetails)
 
 export default linkRoutes;
