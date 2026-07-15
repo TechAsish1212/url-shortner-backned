681f98c651fa3b331ff7005c016b839a5c3d28c2
TechAsish1212 <gitasish1212@gmail.com>
2026-07-16 00:02:13 +0530

    feat: add routes for delete link

--- src/routes/link.route.ts
+++ src/routes/link.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { protect } from "../middlewares/auth.middleware";
-import { createShortUrl, getAllUserLinks, getLinkAnalytics, getLinksDetails, redirectToOriginalUrl } from "../controllers/link.controller";
+import { createShortUrl, deleteLink, getAllUserLinks, getLinkAnalytics, getLinksDetails, redirectToOriginalUrl } from "../controllers/link.controller";
 
 const linkRoutes=Router();
 
@@ -9,5 +9,6 @@ linkRoutes.get('/:shortCode',redirectToOriginalUrl);
 linkRoutes.get('/user/links',protect,getAllUserLinks);
 linkRoutes.get('/user/links/:id',protect,getLinksDetails)
 linkRoutes.get('/user/links/:id/analytics',protect,getLinkAnalytics);
+linkRoutes.delete('/user/links/:id',protect,deleteLink);
 
 export default linkRoutes;
