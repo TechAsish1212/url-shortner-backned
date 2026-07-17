8bf8471b5f7b7cbdf414c906ea4151c9c97ee882
TechAsish1212 <gitasish1212@gmail.com>
2026-07-17 13:49:22 +0530

    feat : ad routes for clickEvent

--- src/routes/clickEvent.route.ts
+++ src/routes/clickEvent.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { trackClient } from "../controllers/clickEvent.controller";
+import { getLinkClicksEvent, trackClient } from "../controllers/clickEvent.controller";
+import { protect } from "../middlewares/auth.middleware";
 
 const clickRouter=Router();
 
 clickRouter.get('/track/:shortCode',trackClient);
+clickRouter.get('/link/:linkId',protect,getLinkClicksEvent);
 
 export default clickRouter;
