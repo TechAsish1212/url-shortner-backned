6a51d9dabd13b6449c628e06778b5555f2a4f43f
TechAsish1212 <gitasish1212@gmail.com>
2026-07-18 13:05:27 +0530

    feat: add analytics routes

--- src/routes/clickEvent.route.ts
+++ src/routes/clickEvent.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getLinkClicksEvent, trackClient } from "../controllers/clickEvent.controller";
+import { getClickAnalytics, getLinkClicksEvent, trackClient } from "../controllers/clickEvent.controller";
 import { protect } from "../middlewares/auth.middleware";
 
 const clickRouter=Router();
 
 clickRouter.get('/track/:shortCode',trackClient);
 clickRouter.get('/link/:linkId',protect,getLinkClicksEvent);
+clickRouter.get('/link/:linkId/analytics',protect,getClickAnalytics); 
 
 export default clickRouter;
